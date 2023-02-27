3aaab68945a912d54b6f9472a5952ea251395612
Fahad Guled <fahad.guled@yahoo.com>
2023-02-26 23:22:58 -0800

    refactored more components. also created layout components and updated head meta for each page.

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -1,23 +1,12 @@
-import Link from "next/link";
-import Navbar from "../../../components/NavBar";
-import Header from "../components/Header";
 import NavBar from "../components/NavBar";
 import MenuCard from "./components/MenuCard";
 export default function RestaurantMenu() {
   return (
-    <main className="bg-gray-100 min-h-screen w-screen">
-      <main className="max-w-screen-2xl m-auto bg-white">
-        <Navbar />
-        <Header />
-        {/* DESCRIPTION PORTION */}
-        <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
-          <div className="bg-white w-[100%] rounded p-3 shadow">
-            <NavBar />
-            <MenuCard />
-          </div>
-        </div>
-        {/* DESCRIPTION PORTION */}
-      </main>
-    </main>
+    <>
+      <div className="bg-white w-[100%] rounded p-3 shadow">
+        <NavBar />
+        <MenuCard />
+      </div>
+    </>
   );
 }
--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,6 +1,3 @@
-import Link from "next/link";
-import Navbar from "../../components/NavBar";
-import Header from "./components/Header";
 import Images from "./components/Images";
 import NavBar from "./components/NavBar";
 import ReservationCard from "./components/ReservationCard";
@@ -10,25 +7,18 @@ import RestaurantTitle from "./components/RestaurantTitle";
 import Reviews from "./components/Reviews";
 export default function RestaurantDetails() {
   return (
-    <main className="bg-gray-100 min-h-screen w-screen">
-      <main className="max-w-screen-2xl m-auto bg-white">
-        <Navbar />
-        <Header />
-        {/* DESCRIPTION PORTION */}
-        <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
-          <div className="bg-white w-[70%] rounded p-3 shadow">
-            <NavBar />
-            <RestaurantTitle />
-            <RestaurantRating />
-            <RestaurantDescription />
-            <Images />
-            <Reviews />
-          </div>
-          <div className="w-[27%] relative text-reg">
-            <ReservationCard />
-          </div>
-        </div>
-      </main>
-    </main>
+    <>
+      <div className="bg-white w-[70%] rounded p-3 shadow">
+        <NavBar />
+        <RestaurantTitle />
+        <RestaurantRating />
+        <RestaurantDescription />
+        <Images />
+        <Reviews />
+      </div>
+      <div className="w-[27%] relative text-reg">
+        <ReservationCard />
+      </div>
+    </>
   );
 }
